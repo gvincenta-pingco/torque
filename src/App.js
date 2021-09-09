7c14ab5c8aa3b74048a7505c71f4630f72ec3a73
gvincenta <gilbert11323@gmail.com>
2021-09-09 21:29:02 +1000

    format all documents

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 //internal imports
-import './App.css';
+import "./App.css";
 import OpenTorque from "./OpenTorque";
 import CloseTorque from "./CloseTorque";
 
 function App() {
   return (
-    <div >
-      <div className="GraphItem"> <OpenTorque/></div>
-      <div className="GraphItem"> <CloseTorque/></div>
-
-   
+    <div>
+      <div className="GraphItem">
+        {" "}
+        <OpenTorque />
+      </div>
+      <div className="GraphItem">
+        {" "}
+        <CloseTorque />
+      </div>
     </div>
   );
 }
--- src/OpenTorque.js
+++ src/OpenTorque.js
@@ -1,68 +1,72 @@
 //external imports
-import {Bar} from 'react-chartjs-2';
-import { useMemo, useState } from 'react';
+import { Bar } from "react-chartjs-2";
+import { useMemo } from "react";
 
 //internal imports
-import torqueData from './data.json';
-import { extractTorqueData } from './utils';
+import torqueData from "./data.json";
+import { extractTorqueData } from "./utils";
 
-
-
-
-function OpenTorque(props){
-    //sample data for chartjs data. 
-    const data = useMemo(() => { //memoize data.
-        const {dataset, labels} = extractTorqueData({ //extract relevant data for those that are Open.
-            data: torqueData,
-            currentProfile : 1, 
-            currentDirection  : "Open"
-        })
-        return { //prepare data structure for chartJS
-            labels,
-            datasets: [{
-                label: 'Average open torque', //for Average open torque bar
-                data: dataset,
-                backgroundColor: "#668ac4", //blue color 
-                parsing: {
-                    yAxisKey: 'AverageTorque'
-                }
-            }, {
-                label: 'Last open torque', //for Last open torque bar
-                data: dataset,
-                parsing: {
-                    yAxisKey: 'LastTorque'
-                }
-            }]
-        } 
-    }, [torqueData])
-    return ( <>
-    <h2> OPEN </h2><Bar
-    data={ data}
-    options={{
-
-        scales:{
+function OpenTorque(props) {
+  //sample data for chartjs data.
+  const data = useMemo(() => {
+    //memoize data.
+    const { dataset, labels } = extractTorqueData({
+      //extract relevant data for those that are Open.
+      data: torqueData,
+      currentProfile: 1, //assume that you are only looking at profile 1.
+      currentDirection: "Open",
+    });
+    return {
+      //prepare data structure for chartJS
+      labels,
+      datasets: [
+        {
+          label: "Average open torque", //for Average open torque bar
+          data: dataset,
+          backgroundColor: "#668ac4", //blue color
+          parsing: {
+            yAxisKey: "AverageTorque",
+          },
+        },
+        {
+          label: "Last open torque", //for Last open torque bar
+          data: dataset,
+          parsing: {
+            yAxisKey: "LastTorque",
+          },
+        },
+      ],
+    };
+  }, [torqueData]);
+  return (
+    <>
+      <h2> OPEN </h2>
+      <Bar
+        data={data}
+        options={{
+          scales: {
             xAxis: {
-                display: true, 
-                text: "Valve Position"
+              display: true,
+              text: "Valve Position",
             },
-            yAxis:{
-                display: true, 
-                text: "Required Torque (%)"
-            }
-        },
-        
-        title:{
-        display:true,
-        text:'Open',
-        fontSize:20
-        },
-        legend:{
-            display:true,
-            position:'right'
-        }
-    }}
-  /> 
-  </>);
+            yAxis: {
+              display: true,
+              text: "Required Torque (%)",
+            },
+          },
 
+          title: {
+            display: true,
+            text: "Open",
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
+    </>
+  );
 }
 export default OpenTorque;
