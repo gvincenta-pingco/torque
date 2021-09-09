7c237d622ee9812f867f1004119c2ac87708b6fe
gvincenta <gilbert11323@gmail.com>
2021-09-09 20:28:08 +1000

    design basic layout for 2 graph in main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+//internal imports
 import './App.css';
+import OpenTorque from "./OpenTorque";
+import CloseTorque from "./CloseTorque";
 
 function App() {
   return (
     <div >
+      <div> <OpenTorque/></div>
+      <div> <CloseTorque/></div>
+
    
     </div>
   );
